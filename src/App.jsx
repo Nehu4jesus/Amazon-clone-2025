4455ee8c2832dc20245e447a02dee812bd8aec51
Nehu4jesus <enehemiya@yahoo.com>
2025-03-04 14:28:45 +0300

    carousel is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Carousel from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <div>
       <Header />
+      <Carousel />
     </div>
   );
 }
