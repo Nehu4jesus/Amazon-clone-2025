fe468826aa3f4f3a2f6572cfe3e1c3b1e167e3c5
Nehu4jesus <enehemiya@yahoo.com>
2025-03-14 10:26:25 +0300

    sign in page is finished

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
-import SignUp from "./pages/Auth/SignUp";
+import Auth from "./pages/Auth/Auth";
 import Orders from "./Pages/Orders/Orders";
 import Cart from "./pages/Cart/Cart";
 import Results from "./pages/Results/Results";
@@ -12,7 +12,7 @@ function Routing() {
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/auth" element={<SignUp />} />
+          <Route path="/auth" element={<Auth />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
