af58bc99a32a8b6041aca29248b25b885e9a38b2
Nehu4jesus <enehemiya@yahoo.com>
2025-03-17 11:57:12 +0300

    building api page completed

--- Amazon-clone-2025/src/Router.jsx
+++ Amazon-clone-2025/src/Router.jsx
@@ -6,6 +6,12 @@ import Orders from "./Pages/Orders/Orders";
 import Cart from "./pages/Cart/Cart";
 import Results from "./pages/Results/Results";
 import ProductDetail  from "./pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51R2bSfE2Cv8LxcKq7aKiaw3EQBwLnpPjJN7olPESHqX2qPg9dPcf3N7iy0hkeJBa8mXKZkBrIVTTF8TM81eCgm5s00GgmQawTr"
+);
+
 function Routing() {
   return (
     <div>
@@ -13,6 +19,14 @@ function Routing() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/payment"
+            element={
+              <Elements stripe={stripePromise} >
+                <Payment />
+              </Elements>
+            }
+          />
           <Route path="/orders" element={<Orders />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
